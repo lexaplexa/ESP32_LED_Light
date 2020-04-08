b0c20a7d4b38138166ffe9754c2a3147988a1e62
LeXa <lexxaa@seznam.cz>
2020-04-08 21:56:05 +0200

    Completely reworked http library

--- httplib.py
+++ httplib.py
@@ -1,39 +1,232 @@
-def GetRequest(client):
-    # When no data received, raise exception
-    client.settimeout(0.2)
+request_methods = ["GET","HEAD","POST","PUT","DELETE","CONNECT","OPTIONS","TRACE","PATCH"]
+response_codes = {
+        # 1×× Informational
+        100:"Continue",
+        101:"Switching Protocols",
+        102:"Processing",
+        # 2×× Success
+        200:"OK",
+        201:"Created",
+        202:"Accepted",
+        203:"Non-authoritative Information",
+        204:"No Content",
+        205:"Reset Content",
+        206:"Partial Content",
+        207:"Multi-Status",
+        208:"Already Reported",
+        226:"IM Used",
+        # 3×× Redirection
+        300:"Multiple Choices",
+        301:"Moved Permanently",
+        302:"Found",
+        303:"See Other",
+        304:"Not Modified",
+        305:"Use Proxy",
+        307:"Temporary Redirect",
+        308:"Permanent Redirect",
+        # 4×× Client Error
+        400:"Bad Request",
+        401:"Unauthorized",
+        402:"Payment Required",
+        403:"Forbidden",
+        404:"Not Found",
+        405:"Method Not Allowed",
+        406:"Not Acceptable",
+        407:"Proxy Authentication Required",
+        408:"Request Timeout",
+        409:"Conflict",
+        410:"Gone",
+        411:"Length Required",
+        412:"Precondition Failed",
+        413:"Payload Too Large",
+        414:"Request-URI Too Long",
+        415:"Unsupported Media Type",
+        416:"Requested Range Not Satisfiable",
+        417:"Expectation Failed",
+        418:"I'm a teapot",
+        421:"Misdirected Request",
+        422:"Unprocessable Entity",
+        423:"Locked",
+        424:"Failed Dependency",
+        426:"Upgrade Required",
+        428:"Precondition Required",
+        429:"Too Many Requests",
+        431:"Request Header Fields Too Large",
+        444:"Connection Closed Without Response",
+        451:"Unavailable For Legal Reasons",
+        499:"Client Closed Request",
+        # 5×× Server Error
+        500:"Internal Server Error",
+        501:"Not Implemented",
+        502:"Bad Gateway",
+        503:"Service Unavailable",
+        504:"Gateway Timeout",
+        505:"HTTP Version Not Supported",
+        506:"Variant Also Negotiates",
+        507:"Insufficient Storage",
+        508:"Loop Detected",
+        510:"Not Extended",
+        511:"Network Authentication Required",
+        599:"Network Connect Timeout Error"}
 
-    # Wait for data from client
-    data = ConvertToUtf8(client.recv(1024))
+class Http:
+    def __init__(self):
+        self._port = 80
+        self._route_functions = {}
+        self.request = Request()
+        self.response = Response()
+
+    def run(self, port = 80):
+        self._port = port
+        import usocket
+        import sys
+
+        # Create http server
+        server = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+        try:
+            server.bind(('', port))
+        except:
+            print('# Bind failed. ')
+            sys.exit()
+        server.listen(5)
+
+        while True:
+            client, addr = server.accept()
+            print("["+str(addr[0])+":"+str(addr[1])+"]: Connected")
+
+            # When no data received, raise exception
+            client.settimeout(0.2)
+            try:
+                data = client.recv(1024)
+            except:
+                client.close()
+                print("["+str(addr[0])+":"+str(addr[1])+"]: No request. Disconnected")
+                continue
+            
+            # Parse received data
+            self.request.parse(data)
+            print("["+str(addr[0])+":"+str(addr[1])+"]: Method:  " + self.request.method)
+            print("["+str(addr[0])+":"+str(addr[1])+"]: Url:     " + self.request.url)
+            print("["+str(addr[0])+":"+str(addr[1])+"]: Header:  " + str(self.request.headers))
+            print("["+str(addr[0])+":"+str(addr[1])+"]: Content: " + str(self.request.content))
+
+            # Call routed funtion
+            try:
+                self.response.body = self._route_functions[self.request.url]()
+            except KeyError:
+                client.sendall("Unknown URL")
+                client.close()
+                print("["+str(addr[0])+":"+str(addr[1])+"]: Unknown url. Disconnected")
+                continue
+
+            # Send response
+            client.sendall(self.response.create())
+
+            client.close()
+            print("["+str(addr[0])+":"+str(addr[1])+"]: Disconnected")
+
+    def route(self, url):
+        def wrap(f):
+            self._route_functions[url] = f
+            return f
+        return wrap
+
+class Request():
+    def __init__(self):
+        self.method = ""
+        self.url = ""
+        self.version = ""
+        self.headers = {}
+        self.content = ""
+
+    def parse(self, data):
+        # Set to default
+        self.method = ""
+        self.url = ""
+        self.version = ""
+        self.headers = {}
+        self.content = ""
+
+        # Convert UTF-8 characters to bytes
+        data = self._convert_request_utf8(data)
+
+        # Separate http header from body
+        data = data.decode('utf-8').split("\r\n\r\n")
+
+        # Split head to rows
+        rows = data[0].split("\r\n")
+
+        try:
+            # First row is different from other rows
+            self.method, self.url, self.version = rows[0].split()
+        except:
+            return
+
+        # Create a dictionary from remaining rows
+        for header in rows[1:]: 
+            header = header.split(":")
+            self.headers.update({header[0].strip():header[1].strip()})
+
+        # Check methods which sending content
+        if self.method == "POST":
+            content_type = self.get_header("Content-Type")
+            if content_type == "application/x-www-form-urlencoded":
+                content = data[1].split("&")
+                temp = []
+                for element in content: 
+                    temp.extend(element.split("="))
+                    self.content = {temp[i]:temp[i+1] for i in range(0,len(temp),2)}
+            else:
+                self.content = data[1]
+
+        return
+
+    def get_header(self, key):
+        try:
+            return self.headers[key]
+        except KeyError:
+            return None
+
+    def _convert_request_utf8(self, data):
+        # Convert UTF-8 characters to bytes
+        # Example: "%C3%BD" >> b'\xc3\xbd'
+        while(1):
+            pos = data.find(b'%')
+            if pos == -1:
+                return data
+            data = data[:pos]+bytes([int(data[pos+1:pos+3],16)])+data[pos+3:]
+
+class Response():
+    def __init__(self):
+        self.code = 200
+        self.headers = {"Content-Type":"text/html"}
+        self.body = ""
     
-    # Separate http header from body
-    header = data.decode('utf-8').split("\r\n\r\n")[0]
-    header = header.split("\n")
-    try:
-        method, link, html = header[0].split()
-    except:
-        return None, None, None
+    def set_header(self, name, value):
+        self.headers.update({name:value})
+    
+    def create(self):
+        # Status line
+        response = "HTTP/1.1 " + str(self.code) + " " + str(response_codes[self.code]) + "\r\n"
 
-    body = data.decode('utf-8').split("\r\n\r\n")[1]
-    print("Method: "+method+"\nLink: "+link+"\nBody: "+body)
-    return method, link, body
+        # Headers
+        for key in self.headers.keys():
+            response += key + ":" + self.headers[key] + "\r\n"
 
-def SendResponse(client, content_type, string):
-    response = "HTTP/1.1 200 OK\nContent-Type: "+ content_type +"\nConnection: close\n\n" + string
-    client.sendall(response)
-    client.close()
-    return
+        # Body
+        response += "\r\n" + self.body
 
-def SendError(client, error_number, message):
-    response = "HTTP/1.1 {} \nContent-Type: text/html\nConnection: close\n\n<H1>{} {}</H1>".format(error_number,error_number,message)
-    client.sendall(response)
-    client.close()
-    return
+        return response
+    
+    def render_template(self, html_file, elements = {}):
+        try:
+            page = open(html_file, "r")
+        except FileNotFoundError:
+            return ""
+        
+        body = page.read()
 
-def ConvertToUtf8(data):
-    # Convert UTF-8 characters to bytes
-    # Example: "%C3%BD" >> b'\xc3\xbd'
-    while(1):
-        pos = data.find(b'%')
-        if pos == -1:
-            return data
-        data = data[:pos]+bytes([int(data[pos+1:pos+3],16)])+data[pos+3:]
+        for key in elements.keys():
+            body = body.replace("{" + key + "}", str(elements[key]))
+
+        return body
--- main.py
+++ main.py
@@ -1,118 +1,44 @@
-import usocket
-import sys
-import httplib
-import light
-import pages
-import _thread
+from httplib import Http
 import ujson
-import time
+import light
 
-# Create web server
-server = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
-try:
-    server.bind(('', 80))
-except:
-    print('# Bind failed. ')
-    sys.exit()
-server.listen(5)
+app = Http()
 
-while True:
-    client, addr = server.accept()
-    print("--------------------------------------------------------")
-    print("Connected to "+str(addr[0])+":"+str(addr[1]))
-    
-    # Handle client request -----------------------------------------------------------------------
-    try:
-        method, link, body = httplib.GetRequest(client)
-    except:
-        client.close()
-        print("Client disconnected")
-        continue
+@app.route("/")
+def index():
+    settings = ujson.load(open("data/settings.json","r"))
+    elements = {"style": open("html/style.css","r").read(),"room":settings["Name"]}
 
-    # GET method ----------------------------------------------------------------------------------
-    if method == "GET":
-        if link == "/":
-            httplib.SendResponse(client, "text/html", pages.index(light.status))
+    if light.status == "ON":
+        elements.update({"class_div_light":"lighton"})
+    else:
+        elements.update({"class_div_light":"lightoff"})
 
-        elif link == "/?led=on":
-            _thread.start_new_thread(light.on,())
-            httplib.SendResponse(client, "text/html", pages.index("ON"))
-        
-        elif link == "/?led=off":
-            _thread.start_new_thread(light.off,())
-            httplib.SendResponse(client, "text/html", pages.index("OFF"))
+    return app.response.render_template("html/index.html", elements)
 
-        elif link == "/settings":
-            httplib.SendResponse(client, "text/html", pages.settings())
-        
-        elif link == "/connection":
-            httplib.SendResponse(client, "text/html", pages.connection())
-        
-        elif link == "/html/style.css":
-            httplib.SendResponse(client, "text/css", open("html/style.css","r").read())
+@app.route("/settings")
+def settings():
+    settings = ujson.load(open("data/settings.json","r"))
+    if app.request.method == "POST":
+        settings["Name"]    = app.request.content["Name"]
+        settings["Timeout"] = int(app.request.content["Timeout"])
+        settings["Max"]     = int(app.request.content["Max"])
+        settings["Rise"]    = int(app.request.content["Rise"])
+        settings["Fall"]    = int(app.request.content["Fall"])
+        settings_file = open("data/settings.json","w")
+        ujson.dump(settings, settings_file)
+        settings_file.close()
 
-        # API -------------------------------------------------------------------------------------
-        elif "/api/" in link:
-            api = link.split("/api/")[1]
-            if api == "ledon":
-                _thread.start_new_thread(light.on, ())
-                httplib.SendResponse(client, "application/json", """{"status":"ON"}""")
-            elif api == "ledoff":
-                _thread.start_new_thread(light.off, ())
-                httplib.SendResponse(client, "application/json", """{"status":"OFF"}""")
-            elif api == "status":
-                httplib.SendResponse(client, "application/json", 
-                    "{{\"status\":\"{}\",\"previous_on\":{}, \"current_on\":{}}}".format(
-                        light.status,
-                        light.previous_on,
-                        int(time.time() - light.current_on) if light.current_on > 0 else 0))
-            elif api == "settings":
-                httplib.SendResponse(client, "application/json", open("data/settings.json","r").read())
-            else:
-                httplib.SendResponse(client, "application/json", """{"error":"not defined"}""")
+    elements = {
+        "style":            open("html/style.css","r").read(),
+        "room":             settings["Name"],
+        "settings_name":    settings["Name"],
+        "settings_timeout": settings["Timeout"],
+        "settings_max":     settings["Max"],
+        "settings_rise":    settings["Rise"],
+        "settings_fall":    settings["Fall"]}
+    return app.response.render_template("html/settings.html", elements)
 
-        else:
-            httplib.SendError(client, 404,"Not found")
-
-    # POST method ---------------------------------------------------------------------------------
-    elif method == "POST":
-        if link == "/settings":
-            # Split body to dictionary
-            body = body.split("&")
-            temp = []
-            for element in body: temp.extend(element.split("="))
-            settings = {temp[i]:temp[i+1] for i in range(0,len(temp),2)}
-            # Convert to integer
-            settings["Max"] = int(settings["Max"])
-            settings["Timeout"] = int(settings["Timeout"])
-            settings["Rise"] = int(settings["Rise"])
-            settings["Fall"] = int(settings["Fall"])
-            # Save to file
-            settings_file = open("data/settings.json","w")
-            ujson.dump(settings, settings_file)
-            settings_file.close()
-            
-            httplib.SendResponse(client, "text/html", pages.settings())
-        
-        elif link == "/connection":
-            # Split body to dictionary
-            body = body.split("&")
-            temp = []
-            for element in body: temp.extend(element.split("="))
-            connection = {temp[i]:temp[i+1] for i in range(0,len(temp),2)}
-            # Save to file
-            connection_file = open("data/connection.json","w")
-            ujson.dump(connection, connection_file)
-            connection_file.close()
-            
-            httplib.SendResponse(client, "text/html", pages.connection())
-
-        # API -------------------------------------------------------------------------------------
-        elif "/api/" in link:
-            api = link.split("/api/")[1]
-            httplib.SendResponse(client, "application/json", """{"error":"not defined"}""")
-        
-        else:
-            httplib.SendError(client, 404,"Not found")
-
-    client.close()
+# Here starts application
+if __name__ == "__main__":
+    app.run()
